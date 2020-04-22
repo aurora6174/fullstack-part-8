bd8c4d34662b889e56baeb6c2807b5ec930e4161
mysticis <eshokhlah@gmail.com>
2020-04-22 01:50:33 +0100

    implement all authors query

--- library-backend.js
+++ library-backend.js
@@ -88,17 +88,19 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author]!
   }
   type Author {
     name: String!
-    id: ID!
-    born: Int
+    bookCount: Int
   }
+
   type Book {
     title: String!
     published: Int!
     author: String!
     genres: [String!]!
+    id: ID!
   }
 `
 
@@ -107,6 +109,12 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    name: (root) => root.name,
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length,
   },
 }
 
